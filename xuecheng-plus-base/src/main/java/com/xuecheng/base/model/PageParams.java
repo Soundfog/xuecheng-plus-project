d089c9c3389dad6641b84913aa87065e2164ecf5
Soundfog <529540907@qq.com>
2023-06-13 17:44:54 +0800

    设计接口以及生成接口文档

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,35 @@
+package com.xuecheng.base.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 分页查询通用参数
+ */
+@Data
+@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class PageParams {
+
+    //当前页码
+    @ApiModelProperty("页码")
+    private Long pageNo = 1L;
+
+    //每页记录数默认值
+    @ApiModelProperty("每页记录数")
+    private Long pageSize = 10L;
+
+    public PageParams() {
+
+    }
+
+    public PageParams(long pageNo, long pageSize) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+
+}
